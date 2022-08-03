90288e1e40b8aaa1cd1718ae4c5f092fdce91c81
Snehal Bagekar <snehal_bagekar@epam.com>
2022-08-03 21:04:07 +0530

    Implemented Multithreading

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -12,34 +12,26 @@ namespace Multithreading
         public static void Method1()
         {
 
-            // It prints numbers from 0 to 10
-            for (int I = 0; I <= 10; I++)
+            for (int i = 0; i <= 10; i++)
             {
-                Console.WriteLine("Method1 is : {0}", I);
+                Console.WriteLine("Method1 is : {0}", i);
 
-                // When the value of I is equal to 5 then
-                // this method sleeps for 6 seconds
-                if (I == 5)
+                if (i == 5)
                 {
                     Thread.Sleep(6000);
                 }
             }
         }
-
-        // static method two
         public static void Method2()
         {
-            // It prints numbers from 0 to 10
-            for (int J = 0; J <= 10; J++)
+            for (int j = 0; j <= 10; j++)
             {
-                Console.WriteLine("Method2 is : {0}", J);
+                Console.WriteLine("Method2 is : {0}", j);
             }
         }
 
-        // Main Method
         public static void Main()
         {
-            // Creating and initializing threads
             Thread thr1 = new Thread(Method1);
             Thread thr2 = new Thread(Method2);
             thr1.Start();
---
+++ ThreadSafety/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSafety
+{
+    public class Program
+    {
+        static Dictionary<int, string> items = new Dictionary<int, string>();
+        static void Main(string[] args)
+        {
+            var task1 = Task.Factory.StartNew(AddItem);
+            var task2 = Task.Factory.StartNew(AddItem);
+            var task3 = Task.Factory.StartNew(AddItem);
+            var task4 = Task.Factory.StartNew(AddItem);
+            var task5 = Task.Factory.StartNew(AddItem);
+
+            Task.WaitAll(task3, task2, task1, task4, task5);
+
+            Console.ReadKey();
+        }
+
+        private static void AddItem()
+        {
+            //if (items.ContainsKey(1))
+            //{
+
+            //}
+            //else
+            //{
+            //    items.Add(1, "Hello World");
+            //}
+
+            lock (items)
+            {
+                Console.WriteLine("Lock acquired by " + Task.CurrentId);
+                items.Add(items.Count, "ABC " + items.Count);
+            }
+
+            Dictionary<int, string> dictionary;
+
+            lock (items)
+            {
+                Console.WriteLine("Lock 2 acquired by " + Task.CurrentId);
+                dictionary = items;
+            }
+            foreach (var kvp in dictionary)  //kvp --> Key-Value Pair
+            {
+                Console.WriteLine(kvp.Key + " " + kvp.Value);
+            }
+        }
+    }
+}
