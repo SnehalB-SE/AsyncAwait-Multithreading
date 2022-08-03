b0290b4669c7984b2fbec5a434c385e2cd40552b
Snehal Bagekar <snehal_bagekar@epam.com>
2022-08-03 09:13:28 +0530

    Add project files.

---
+++ AsyncAwaitBySweta/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public class Program
+    {
+		static readonly Stopwatch timer = new Stopwatch();
+		public static async Task Main()
+		{
+			Program obj = new Program();
+			await obj.ProcessAsync();
+			Console.ReadKey();
+		}
+		public async Task ProcessAsync()
+		{
+			timer.Start();
+			var data = await GetDataFromDBAsync();
+			Console.WriteLine("In the Main Method " + data + " " + timer.ElapsedMilliseconds.ToString());
+			var processedData = await ProcessDataAsync(data);
+			await WriteToDBAsync(processedData);
+		}
+		public static async Task<int> GetDataFromDBAsync()
+        {
+			Console.WriteLine("GetDataFromDBAsync Started..." + timer.ElapsedMilliseconds.ToString());
+			//Thread.Sleep(5000);
+			await Task.Delay(5000);
+			//var ProcessResult = await ProcessDataAsync(100);
+			//Console.WriteLine("Result from ProcessDataAsync: " + ProcessResult + " " + timer.ElapsedMilliseconds.ToString());
+			var a = 0;
+            for (int i = 0; i < 500_000_000; i++)
+            {
+				a += i;
+            }
+			Console.WriteLine("GetDataFromDBAsync Result a = " + a + " " + timer.ElapsedMilliseconds.ToString());
+			return a;
+        }
+		public static async Task<int> ProcessDataAsync(int data)
+		{
+			Console.WriteLine("ProcessDataAsync:: Processing Data : " + data + " " + timer.ElapsedMilliseconds.ToString());
+			await Task.Delay(1000);
+			Console.WriteLine("ProcessDataAsync:: Processing End : " + data + " " + timer.ElapsedMilliseconds.ToString());
+			return 0;
+		}
+		public static async Task<int> WriteToDBAsync(int processedData)
+		{
+			Console.WriteLine("Data");
+			await Task.Delay(1000);
+			return 1;
+		}
+
+	}
+}
---
+++ Multithreading/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    public class Program
+    {
+        public static void Method1()
+        {
+
+            // It prints numbers from 0 to 10
+            for (int I = 0; I <= 10; I++)
+            {
+                Console.WriteLine("Method1 is : {0}", I);
+
+                // When the value of I is equal to 5 then
+                // this method sleeps for 6 seconds
+                if (I == 5)
+                {
+                    Thread.Sleep(6000);
+                }
+            }
+        }
+
+        // static method two
+        public static void Method2()
+        {
+            // It prints numbers from 0 to 10
+            for (int J = 0; J <= 10; J++)
+            {
+                Console.WriteLine("Method2 is : {0}", J);
+            }
+        }
+
+        // Main Method
+        public static void Main()
+        {
+            // Creating and initializing threads
+            Thread thr1 = new Thread(Method1);
+            Thread thr2 = new Thread(Method2);
+            thr1.Start();
+            thr2.Start();
+            Console.ReadKey();
+        }
+    }
+}
